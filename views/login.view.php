67f8a4f292c0993f9806c883ce6c5f8c677bf27f
Jaime Piña <35391287+JimmyDeveloper@users.noreply.github.com>
2021-02-06 17:19:10 -0600

    Add files via upload

---
+++ views/login.view.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PACHUCO &amp; GAMES</title>
+	<link href='https://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+
+	<link href="css/reset.css" rel="stylesheet" > <!-- CSS reset -->
+	<link href="css/style.css" rel="stylesheet" > <!-- Resource style -->
+	<link href="css/demo.css" rel="stylesheet" > <!-- Demo style -->
+	<link href="style/index_style.css" rel="stylesheet" >
+	<link href="style_video/styles.css" rel="stylesheet" >
+
+	<script src="https://use.fontawesome.com/5fe7ff4e50.js"></script>
+</head>
+<body style="background-color:#000000;">
+
+
+		<section class="showcase">
+		<div class="video-container">
+			<video src="media/Bite_Rain_R.mp4" autoplay muted loop></video>
+		</div>
+			<div class="content">
+				<h1>PACHUCO GAMES</h1>
+				<h3>Vive la nueva experiancia</h3>
+				<a href="#about" class="btn">ver mas</a>
+					</div>
+	  </section>
+
+			<div class="cd-main-header__logo"><a href="#0"><img src="img/cd-logo.svg" alt="Logo"></a></div>
+
+			<nav class="cd-main-nav js-main-nav">
+				<ul class="cd-main-nav__list js-signin-modal-trigger">
+					<!-- inser more links here -->
+					<!-- ONLY COPY PASTE-->
+					<li><a class="cd-main-nav__item cd-main-nav__item--signin" href="views/login_0.view.php" >Sign in</a></li>
+					<li><a class="cd-main-nav__item cd-main-nav__item--signup" href="#0">Sign up</a></li>
+				</ul>
+			</nav> <BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR>
+
+		   </form>
+
+
+
+		</div> <!-- cd-signin-modal -->
+
+
+	<script src="js/placeholders.min.js"></script> <!-- polyfill for the HTML5 placeholder attribute -->
+	<script src="js/main_0.js"></script> <!-- Resource JavaScript -->
+
+</body>
+</html>
